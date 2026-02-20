e56ea9d9c25c11611111c063f72870f852fee2d0
Tina Wulff <tina@tinawulff.dk>
2026-02-20 12:41:39 +0100

    forside holdtyper added

--- src/app/components/homeComponents/Hero.jsx
+++ src/app/components/homeComponents/Hero.jsx
@@ -10,7 +10,7 @@
     export default function HeroComp() {
     return (
     <section className="flex flex-col h-[100vh] justify-between z-full max-w-full">
-        <Image fill className="absolute h-[100vh] w-full object-cover" src={heroImg} alt="Hero Image" />
+        <Image width={1499} height={1000} className="absolute h-[100vh] w-full object-cover" src={heroImg} alt="Hero Image" />
         <Image width={600} height={400} className="relative z-10 mt-20 max-w-[90%]" src={landrupLogo} alt="Landrup Logo" />
         <div className="flex flex-col mx-20">
         <button className="bg-[#E9E9E9] text-[#003147] text-lg align-center items-center p-4 rounded-lg z-10 h-[53px] w-full max-w-[300px] self-center shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] leading-none relative align-end">Log ind her</button>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,32 @@
 import HeroComp from "./components/homeComponents/Hero";
+import TeamTypes from "./components/homeComponents/TeamTypes";
 
-// import boerneDans from "../../assets/boernedans.jpg";
-// import seniorDans from "../../assets/seniordans.jpg";
-// import moderneDans from "../../assets/modernedans.jpg";
-// import streetDans from "../../assets/streethiphop.jpg";
+// HoldTyper - Images & objekts til holdtyperne på forsiden.
+import boerneDans from "../assets/boernedans.jpg";
+import seniorDans from "../assets/seniordans.jpg";
+import moderneDans from "../assets/modernedans.jpg";
+import streetDans from "../assets/streethiphop.jpg";
+
+const articles = [
+  { title: "Børnehold", img: boerneDans, text: "På børneholdene leger vi os ind i dansens verden gennem musik, bevægelse og fantasi. Undervisningen styrker motorik, rytme og kropsbevidsthed i trygge rammer. Fokus er på danseglæde, fællesskab og aktiv bevægelse, hvor alle kan være med."},
+  { title: "Selskabs- og seniordans", img: seniorDans, text: "Selskabs- og seniordans kombinerer hyggeligt samvær med skånsom motion. Vi danser klassiske pardanse i et tempo, hvor alle kan følge med. Undervisningen styrker balance, koordination og kondition, samtidig med at fællesskabet og danseglæden er i centrum." },
+  { title: "Moderne dans og ballet", img: moderneDans, text: "Moderne dans og ballet forener teknik, kropskontrol og musikalsk udtryk. Træningen forbedrer styrke, smidighed og holdning gennem varierede øvelser. Undervisningen foregår i en positiv atmosfære, hvor bevægelsesglæde og koncentration skaber både fordybelse og effektiv motion." },
+  { title: "Streetdance og hiphop", img: streetDans, text: "Streetdance og hiphop er energifyldt træning med fokus på rytme, attitude og fællesskab. Vi arbejder med grooves, koreografier og grundtrin, der styrker kondition og koordination. Stemningen er uformel og motiverende, så motion og danseglæde går hånd i hånd." },
+];
 
 
  export default function Home() {
    return (
   <>
   <HeroComp />
+
+  <section className="my-10">
+    <h1 className="mx-6 text-4xl mb-8">Vores holdtyper</h1>
+    {articles.map((item, idx) => (
+    <TeamTypes key={idx} title={item.title} text={item.text} img={item.img} />
+    ))}
+  </section>
+
   </>
-
-
    );
  }
